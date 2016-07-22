b89820b553ca3b83b5dfeb739ce5a19d44b349f4
web1992 <554933654@qq.com>
2016-07-22 17:26:06 +0800

    add [velocity 开发环境集成]

---
+++ app/src/main/java/cn/web1992/controller/IndexController.java
@@ -0,0 +1,30 @@
+package cn.web1992.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index.html")
+    public String render(ModelMap map, HttpServletRequest request) {
+        return "index";
+    }
+
+
+    /**
+     * 这里让spring 来控制主页，web.xml 的 welcome-file-list  不起作用
+     * @return index.vm
+     */
+    @RequestMapping(value = {" ", "/"})
+    public ModelAndView index() {
+        return new ModelAndView("index");// index.vm
+    }
+
+
+}
