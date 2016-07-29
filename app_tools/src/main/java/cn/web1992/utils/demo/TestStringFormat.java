751c1022fa699636261e8b5262cff20dab917f70
web1992 <554933654@qq.com>
2016-07-29 17:21:49 +0800

    add [字符串工具类]

--- app_tools/src/main/java/cn/web1992/utils/demo/TestStringFormat.java
+++ app_tools/src/main/java/cn/web1992/utils/demo/TestStringFormat.java
@@ -2,6 +2,10 @@ package cn.web1992.utils.demo;
 
 import cn.web1992.utils.srtutils.StringFormatUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Created by web1992 on 2016/7/29.
  *
@@ -12,5 +16,21 @@ public class TestStringFormat {
 
         System.out.println(StringFormatUtil.format("{%s},{%s}", "a", "b"));
 
+        List<Integer> list = new ArrayList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.forEach(o -> System.out.println(o));
+        System.out.println("===");
+        list.stream().filter(o -> Integer.valueOf(o) == 1).forEach(o -> System.out.print(o));
+
+        list.stream().filter(o -> Integer.valueOf(o) == 1).forEach(System.out::println);
+
+        list.stream().filter(o -> Integer.valueOf(o) == 1).forEach(System.out::println);
+
+        Stream.generate(Math::random).limit(5).forEach(System.out::println);
+
     }
+
 }
