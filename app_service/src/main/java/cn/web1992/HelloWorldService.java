ad93223e06a72ca08bb79fe68fcc11ed05c4ba60
web1992 <554933654@qq.com>
2016-08-09 08:53:47 +0800

    add [hello world]

---
+++ app_service/src/main/java/cn/web1992/HelloWorldService.java
@@ -0,0 +1,10 @@
+package cn.web1992;
+
+/**
+ * Created by web1992 on 2016/8/9.
+ *
+ * @desc
+ */
+public interface HelloWorldService {
+    public void sayHello();
+}
---
+++ app_service_impl/src/main/java/cn/web1992/HelloWorldServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.web1992;
+
+/**
+ * Created by web1992 on 2016/8/9.
+ *
+ * @desc
+ */
+public class HelloWorldServiceImpl implements HelloWorldService {
+
+
+    @Override
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
