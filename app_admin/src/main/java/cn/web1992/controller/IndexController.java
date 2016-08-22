316ec24425a55e33d6eab3f95981de8bda58652e
web1992 <554933654@qq.com>
2016-08-22 14:57:34 +0800

    add [项目结构优化 & 添加 app_admin]

---
+++ app_admin/src/main/java/cn/web1992/controller/IndexController.java
@@ -0,0 +1,35 @@
+package cn.web1992.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by web1992 on 2016/8/22.
+ *
+ * @desc
+ */
+@Controller
+public class IndexController {
+
+
+    @RequestMapping("/index.html")
+    public String render(ModelMap map, HttpServletRequest request) {
+        return "index";
+    }
+
+
+    /**
+     * 这里让spring 来控制主页，web.xml 的 welcome-file-list  不起作用
+     * @return index.vm
+     */
+    @RequestMapping(value = {" ", "/"})
+    public ModelAndView index() {
+        return new ModelAndView("index");// index.vm
+    }
+
+
+}
