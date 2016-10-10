00a7e5dd856197f51cf0459dd2e0956a90ed7420
web1992 <554933654@qq.com>
2016-10-10 14:09:21 +0800

    add [spring mvc 获取path 变量]

---
+++ app_client/src/main/java/cn/web1992/controller/pathvariable/PathVariableController.java
@@ -0,0 +1,21 @@
+package cn.web1992.controller.pathvariable;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by web1992 on 2016/10/10.
+ *
+ * @desc spring mvc 获取path 变量
+ */
+@Controller
+public class PathVariableController {
+
+    @RequestMapping(value = "/path/{name}")
+    @ResponseBody
+    public String pathVariable(@PathVariable String name) {
+        return String.format("name=%s,age+5s", name);
+    }
+}
