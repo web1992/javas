4cb879c593e3128614711b273cc28caec52f22e9
web1992 <554933654@qq.com>
2016-11-29 14:49:11 +0800

    add [spring mvc]

---
+++ app_new/src/main/java/cn/web1992/controller/IndexController.java
@@ -0,0 +1,20 @@
+package cn.web1992.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class IndexController {
+    /**
+     * 这里让spring 来控制主页，web.xml 的 welcome-file-list  不起作用
+     * @return index.vm
+     */
+    @RequestMapping(value = {" ", "/"})
+    @ResponseBody
+    public String index() {
+        return "200";
+    }
+
+}
