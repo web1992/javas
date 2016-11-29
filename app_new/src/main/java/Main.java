95df5f5a8f9b65115b8f4f702e091be59574af1f
web1992 <554933654@qq.com>
2016-11-29 11:18:08 +0800

    add [new app with jetty & maven build]

---
+++ app_new/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/11/29.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app_new/src/main/java/cn/web1992/utils/AppConfig.java
@@ -0,0 +1,18 @@
+package cn.web1992.utils;
+
+/**
+ * Created by Administrator on 2016/11/29.
+ */
+public class AppConfig {
+
+    private boolean isTest;
+
+
+    public boolean isTest() {
+        return isTest;
+    }
+
+    public void setTest(boolean test) {
+        isTest = test;
+    }
+}
