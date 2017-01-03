9b9deb4c4934d18af00b6086aeabc79e2ecb1c50
web1992 <554933654@qq.com>
2017-01-03 17:33:45 +0800

    fix [SynchronizedVsLock]

--- app_tools/src/main/java/cn/web1992/utils/demo/thread/SynchronizedVsLock.java
+++ app_tools/src/main/java/cn/web1992/utils/demo/thread/SynchronizedVsLock.java
@@ -1,5 +1,6 @@
 package cn.web1992.utils.demo.thread;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -7,7 +8,13 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author web1992
  * @deta 2017/1/3
- * @desc
+ * @desc synchronized 与 Lock 用法区别
+ *
+ * @see  Lock#lock() 获取锁，如果没有获取到锁，就会一直等待
+ * @see  Lock#unlock() 在获取锁之后，必须手动释放锁
+ *
+ * @see  Lock#tryLock()  尝试获取锁，获取到锁，立即返回true
+ * @see  Lock#tryLock(long, TimeUnit) 尝试获取锁,等待一段时间
  */
 public class SynchronizedVsLock {
     public static void main(String[] args) {
