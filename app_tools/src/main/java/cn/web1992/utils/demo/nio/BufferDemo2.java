72bd17f6c8e15812323ef7cee1b9e4f29d66d966
web1992 <554933654@qq.com>
2018-10-29 16:20:01 +0800

    add [buffer demo]

---
+++ app_tools/src/main/java/cn/web1992/utils/demo/nio/BufferDemo2.java
@@ -0,0 +1,25 @@
+package cn.web1992.utils.demo.nio;
+
+import java.nio.CharBuffer;
+
+public class BufferDemo2 {
+    public static void main(String[] args) {
+        String[] poem = {"Roses are red", "Violets are blue", "Sugar is sweet", "And so are you."};
+        CharBuffer buffer = CharBuffer.allocate(50);
+        for (int i = 0; i < poem.length; i++) {
+            // Fill the buffer.
+            for (int j = 0; j < poem[i].length(); j++) {
+                buffer.put(poem[i].charAt(j));
+            }
+            // Flip the buffer so that its contents can be read.
+            buffer.flip();
+            // Drain the buffer.
+            while (buffer.hasRemaining()) {
+                System.out.print(buffer.get());
+            }
+            // Empty the buffer to prevent BufferOverflowException.
+            buffer.clear();
+            System.out.println();
+        }
+    }
+}
