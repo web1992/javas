5162e3c825d1b76a1aabd48bf5964f3ac692f5b9
web1992 <554933654@qq.com>
2019-02-20 17:51:17 +0800

    fix CyclicBarrierDemo

--- tools/src/main/java/cn/web1992/utils/demo/thread/CyclicBarrierDemo.java
+++ tools/src/main/java/cn/web1992/utils/demo/thread/CyclicBarrierDemo.java
@@ -6,15 +6,19 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.IntStream;
 
+/**
+ * desc: 模拟一个4人斗地主的场景，4个人都到了，斗地主开始
+ */
 public class CyclicBarrierDemo {
     public static void main(String[] args) {
 
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, () -> System.out.println("done ..."));
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(4, () -> System.out.println("开始斗地主 ..."));
 
-        IntStream.range(0, 5).forEach(i -> new Thread(() -> {
+        IntStream.range(0, 4).forEach(i -> new Thread(() -> {
             try {
+                System.out.println("等待开局 ..." + i);
                 cyclicBarrier.await(1, TimeUnit.SECONDS);
-                System.out.println("await done ..." + i);
+                System.out.println("斗地主开始了 ..." + i);
             } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
                 e.printStackTrace();
             }
