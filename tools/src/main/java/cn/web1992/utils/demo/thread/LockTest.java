3aed95904989820924b9e84bdf2dd00849569b8e
erbao.wang <554933654@qq.com>
2019-06-28 19:02:01 +0800

    add lock test

--- tools/src/main/java/cn/web1992/utils/demo/thread/LockTest.java
+++ tools/src/main/java/cn/web1992/utils/demo/thread/LockTest.java
@@ -1,17 +1,41 @@
 package cn.web1992.utils.demo.thread;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockTest {
     public static void main(String[] args) throws InterruptedException {
 
         ReentrantLock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+        Thread t = new Thread(() -> {
+            try {
+                reentrantLock.lock();
+                System.out.println("await start");
+                condition.await();
+                System.out.println("await end");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                reentrantLock.unlock();
+            }
+        });
+        t.start();
 
-        reentrantLock.lock();
-        // 在这里处理共享资源
-        System.out.println("locking ...");
-        reentrantLock.unlock();
-        System.out.println("end");
+        // System.out.println("main run");
+        // System.out.println("interrupt");
+        t.interrupt();
+
+        try {
+            reentrantLock.lock();
+            System.out.println("main signal");
+            condition.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
+        }
+
 
     }
 }
