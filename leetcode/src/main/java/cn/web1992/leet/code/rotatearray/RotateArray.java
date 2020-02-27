95237478e8ed12b2349f87d572efb56ee733ac1a
web1992 <554933654@qq.com>
2020-02-27 22:57:03 +0800

    add leetcode RotateArray

---
+++ leetcode/src/main/java/cn/web1992/leet/code/rotatearray/RotateArray.java
@@ -0,0 +1,32 @@
+package cn.web1992.leet.code.rotatearray;
+
+import java.util.Arrays;
+
+/**
+ * @author web1992
+ * @date 2020/2/27  22:51
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+
+        int[] ints = rotateArray(new int[]{1, 5, 2, 3, 4, 5, 0, 0, 0}, 5);
+        // [4, 5, 0, 0, 0, 1, 5, 2, 3]
+        System.out.println(Arrays.toString(ints));
+    }
+
+    private static int[] rotateArray(int[] arr, int k) {
+
+        if (arr == null) {
+            return null;
+        }
+
+        int[] newArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            newArr[(i + k) % arr.length] = arr[i];
+        }
+
+        return newArr;
+
+    }
+}
