68eef7c551ccad1c297551590cf1304c1e290e89
web1992 <554933654@qq.com>
2021-12-29 16:22:56 +0800

    fix [BubbleSort2.java]

--- leetcode/src/main/java/cn/web1992/sort/bubble/sort/BubbleSort2.java
+++ leetcode/src/main/java/cn/web1992/sort/bubble/sort/BubbleSort2.java
@@ -21,10 +21,9 @@ public class BubbleSort2 {
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    // 后移
+                    int t = arr[j + 1];
                     arr[j + 1] = arr[j];
-                    arr[j] = temp;
+                    arr[j] = t;
                 }
             }
         }
