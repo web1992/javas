cc2cfd6c90912b1469aace404bedca8fcdee4d77
web1992 <554933654@qq.com>
2022-03-06 15:43:09 +0800

    add [CountingSort.java]

--- leetcode/src/main/java/cn/web1992/sort/counting/sort/CountingSort.java
+++ leetcode/src/main/java/cn/web1992/sort/counting/sort/CountingSort.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
 public class CountingSort {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 100, 200, 300, 400, 500, 0, 1, 2, 3};
+        //int[] arr = new int[]{1, 2, 100, 200, 300, 400, 500, 0, 1, 2, 3};
+        int[] arr = new int[]{2, 5, 3, 0, 2, 3, 0, 3};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -18,6 +19,7 @@ public class CountingSort {
         int len = arr.length;
         int max = arr[0];
 
+        // 1:第一步
         // 找到最大值
         for (int i = 1; i < arr.length; i++) {
             if (max < arr[i]) {
@@ -25,7 +27,9 @@ public class CountingSort {
             }
         }
 
+        // 2:第二步
         // 初始化最大数据
+        // countArr 的index等于原arr的数值,index下标对应的的是数值在arr中出现的次数
         int[] countArr = new int[max + 1];
 
         // 计数
@@ -38,6 +42,7 @@ public class CountingSort {
             countArr[i] = countArr[i - 1] + countArr[i];
         }
 
+        // 3:第三步
         // 核心
         int[] temp = new int[len];
         for (int i = len - 1; i >= 0; i--) {
@@ -49,6 +54,7 @@ public class CountingSort {
             countArr[arr[i]]--;
         }
 
+        // 4:第四步
         // copy 复制
         for (int i = 0; i < len; i++) {
             arr[i] = temp[i];
