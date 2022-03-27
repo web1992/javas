2030923dc44ad6ac4427307c5c1ab2f59e8cb1a1
web1992 <554933654@qq.com>
2022-03-27 16:07:16 +0800

    add [最小覆盖子串2.java]

---
+++ leetcode/src/main/java/cn/web1992/leet/code/最小覆盖子串2.java
@@ -0,0 +1,65 @@
+package cn.web1992.leet.code;
+
+/**
+ * @author web1992
+ * @implNote 最小覆盖子串
+ * @link {https://www.bilibili.com/video/BV1qX4y1V7EC}
+ * @date 2022/2/25  9:29 下午
+ */
+public class 最小覆盖子串2 {
+    public static void main(String[] args) {
+        String target = "ABC";
+        String searchTxt = "ADOBECODEBANC";
+
+        String search = search(searchTxt, target);
+        System.out.println(search);
+        assert "BANC".equalsIgnoreCase(search);
+    }
+
+
+    private static String search(String maxStr, String minStr) {
+
+        if (maxStr.length() <= 0 || maxStr.length() < minStr.length()) {
+            return "";
+        }
+
+        int minLen = minStr.length();
+        int maxLen = maxStr.length();
+        int[] map = new int[128];
+
+        int left = 0;
+        int right = 0;
+        int start = 0;
+        int minWindow = Integer.MAX_VALUE;
+        int count = minLen;
+
+        for (int i = 0; i < minLen; i++) {
+            map[minStr.charAt(i)]++;
+        }
+
+        while (right < maxLen) {
+            char c = maxStr.charAt(right++);// 移动right指针
+            if (map[c]-- > 0) {
+                count--;
+            }
+
+            while (count == 0) {
+                if (right - left < minWindow) {
+                    minWindow = right - left;
+                    start = left;
+                }
+                c = maxStr.charAt(left++);// 移动left指针
+                if (map[c]++ >= 0) {
+                    count++;
+                }
+
+            }
+
+        }
+
+        return minWindow == Integer.MAX_VALUE ? "" : maxStr.substring(start, start + minWindow);
+
+    }
+
+
+}
