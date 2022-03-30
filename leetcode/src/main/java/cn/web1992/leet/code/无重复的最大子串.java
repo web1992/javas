912f76e7d6542b4669bb06717207d053b2a2fde0
web1992 <554933654@qq.com>
2022-03-30 22:00:32 +0800

    add [无重复的最大子串.java]

--- leetcode/src/main/java/cn/web1992/leet/code/无重复的最大子串.java
+++ leetcode/src/main/java/cn/web1992/leet/code/无重复的最大子串.java
@@ -7,30 +7,31 @@ import java.util.Map;
  * @author web1992
  * @date 2022/3/7  9:18 下午
  * @link {https://www.bilibili.com/video/BV1um4y1Z7oE}
+ * @link {https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/}
  */
 public class 无重复的最大子串 {
     public static void main(String[] args) {
 
-        String str = "abcabcbb";
+        String str = "abcbbcbb";
 
-        System.out.println(getMinStr(str));
+        System.out.println(lengthOfLongestSubstring(str));
     }
 
-    private static int getMinStr(String str) {
+    public static int lengthOfLongestSubstring(String str) {
 
         // map 计数
         Map<Character, Integer> map = new HashMap<>();
 
         int begin = 0;
-        int maxLength = -1;
+        int maxLength = 0;
 
         for (int j = 0; j < str.length(); j++) {
             char ch = str.charAt(j);
             map.merge(ch, 1, Integer::sum);// 计数+1
 
             while (map.get(ch) > 1) {// 如果值大于2，说明存在重复的，移动begin,计数-1
+                map.merge(str.charAt(begin), -1, Integer::sum);
                 begin++;
-                map.merge(ch, -1, Integer::sum);
             }
             // 每次都计算
             maxLength = Math.max(maxLength, j - begin + 1);
