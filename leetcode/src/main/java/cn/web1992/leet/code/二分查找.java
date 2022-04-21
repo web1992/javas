5cf0eeea2b70668c918e32f7cd48382262dc5519
web1992 <554933654@qq.com>
2022-04-21 09:41:54 +0800

    add [二分查找.java]

---
+++ leetcode/src/main/java/cn/web1992/leet/code/二分查找.java
@@ -0,0 +1,33 @@
+package cn.web1992.leet.code;
+
+/**
+ * @author web1992
+ * @date 2022/4/21  09:26
+ */
+public class 二分查找 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-2, -1, 1, 3, 4, 11, 22, 33, 34, 99};
+
+        int num = find(arr, -2);
+
+        System.out.println("num=" + num);
+    }
+
+    public static int find(int[] arr, int num) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int m = (left + right) / 2;
+            if (arr[m] < num) {
+                left = m + 1;
+            } else if (arr[m] > num) {
+                right = m - 1;
+            } else {
+                return arr[m];
+            }
+        }
+        return -1;
+    }
+}
