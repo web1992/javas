73d6d0d64b4d02020060ab863c2a88c8d6e58860
web1992 <554933654@qq.com>
2022-04-24 22:05:05 +0800

    add [三数之和.java]

--- leetcode/src/main/java/cn/web1992/leet/code/三数之和.java
+++ leetcode/src/main/java/cn/web1992/leet/code/三数之和.java
@@ -16,17 +16,18 @@ public class 三数之和 {
     public static void main(String[] args) {
         int[] arr = new int[]{-1, 0, 1, 2, -1, 4};
 
-        for (List<Integer> sum : new 三数之和().threeSum(arr)) {
+        for (List<Integer> sum : threeSum(arr)) {
             System.out.println(sum.toString());
         }
     }
 
     // 双指针
-    public List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum(int[] nums) {
 
         List<List<Integer>> list = new ArrayList<>();
         // 排序
-        int[] arrSorted = Arrays.stream(nums).sorted().toArray();
+        Arrays.sort(nums);
+        int[] arrSorted = nums;
 
         for (int i = 0; i < arrSorted.length; i++) {
             int left = i + 1;
