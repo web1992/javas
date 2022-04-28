e794f0413a0ca8df4b416772e00f4fa0456cc21d
web1992 <554933654@qq.com>
2022-04-28 18:49:17 +0800

    add [单词拆分Test001]

--- leetcode/src/main/java/cn/web1992/leet/code/test/单词拆分Test001.java
+++ leetcode/src/main/java/cn/web1992/leet/code/test/单词拆分Test001.java
@@ -1,4 +1,4 @@
-package cn.web1992.leet.code;
+package cn.web1992.leet.code.test;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +9,7 @@ import java.util.List;
  * @link {https://www.bilibili.com/video/BV1wi4y1X7C9}
  * @link {https://leetcode-cn.com/problems/word-break/}
  */
-public class 单词拆分 {
+public class 单词拆分Test001 {
 
     public static void main(String[] args) {
 
@@ -25,18 +25,21 @@ public class 单词拆分 {
     public static boolean wordBreak(String s, List<String> wordDict) {
 
         int len = s.length();
+        // dp[i]=true的含义：字符串长度为i的话，dp[i]为true，表示可以拆分为一个或多个在字典中出现的单词。
         boolean[] dp = new boolean[len + 1];
         dp[0] = true;
 
         for (int i = 1; i <= len; i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                String word = s.substring(j, i);
+                if (dp[j] && wordDict.contains(word)) {
                     dp[i] = true;
                     break;
                 }
             }
         }
 
+        System.out.println(Arrays.toString(dp));
         return dp[len];
 
     }
