f4f38b3d2f0ed1ec240f7f22929fe0766728e26b
web1992 <554933654@qq.com>
2022-05-01 13:07:17 +0800

    add [买卖股票的最佳时机.java]

--- leetcode/src/main/java/cn/web1992/leet/code/test/买卖股票的最佳时机Test03.java
+++ leetcode/src/main/java/cn/web1992/leet/code/test/买卖股票的最佳时机Test03.java
@@ -1,22 +1,23 @@
-package cn.web1992.leet.code;
+package cn.web1992.leet.code.test;
 
 /**
  * @author web1992
  * @date 2022/3/7  11:05 下午
+ * @link {https://www.bilibili.com/video/BV1cS4y137mg}
  * @link {https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/}
  * @implNote 设计一个算法来计算你所能获取的最大利润
  */
-public class 买卖股票的最佳时机 {
+public class 买卖股票的最佳时机Test03 {
     public static void main(String[] args) {
 
         int[] prices = new int[]{7, 1, 5, 3, 6, 4};
 
-        System.out.println(new 买卖股票的最佳时机().maxProfit(prices));
+        System.out.println(new 买卖股票的最佳时机Test03().maxProfit(prices));
 
     }
 
 
-    // 贪心，用左侧最低点作为买入点，比较右侧-左侧最低点的最大值就是所求最大利润
+    // 贪心算法
     public int maxProfit(int[] prices) {
 
         int minPrice = Integer.MAX_VALUE;
--- leetcode/src/main/java/cn/web1992/leet/code/买卖股票的最佳时机2.java
+++ leetcode/src/main/java/cn/web1992/leet/code/买卖股票的最佳时机2.java
@@ -4,7 +4,7 @@ package cn.web1992.leet.code;
  * @author web1992
  * @date 2022/3/7  11:05 下午
  * @link {https://www.bilibili.com/video/BV1cS4y137mg}
- * @link {https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/}
+ * @link {https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/}
  */
 public class 买卖股票的最佳时机2 {
     public static void main(String[] args) {
@@ -16,16 +16,35 @@ public class 买卖股票的最佳时机2 {
     }
 
 
-    // 贪心，用左侧最低点作为买入点，比较右侧-左侧最低点的最大值就是所求最大利润
+    // 动态规划
     public static int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            min = Math.min(min, prices[i]);
-            int profit = prices[i] - min;
-            maxProfit = Math.max(maxProfit, profit);
-        }
 
-        return maxProfit;
+        int len = prices.length;
+        // dp[i][0]=在第i天出股票
+        // dp[i][1]=在第i天入股票
+        int dp0 = 0;
+        int dp1 = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            dp0 = Math.max(dp0, dp1 + prices[i]);
+            dp1 = Math.max(dp1, dp0 - prices[i]);
+        }
+        return dp0;
     }
+
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int[][] dp = new int[n][2];
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+            for (int i = 1; i < n; ++i) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+            return dp[n - 1][0];
+        }
+    }
+
+
 }
