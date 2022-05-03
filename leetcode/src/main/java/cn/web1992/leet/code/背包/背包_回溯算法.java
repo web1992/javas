fb5a9194a77b9330657f1f814fca11b69bf5c1b7
web1992 <554933654@qq.com>
2022-05-03 23:13:38 +0800

    add [背包_回溯算法.java]

---
+++ leetcode/src/main/java/cn/web1992/leet/code/背包/背包_回溯算法.java
@@ -0,0 +1,32 @@
+package cn.web1992.leet.code.背包;
+
+/**
+ * @author web1992
+ * @date 2022/4/30  22:24
+ */
+public class 背包_回溯算法 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //0-1背包问题根据老师下边这句话的讲解，代码再加两行注释就非常容易理解了
+    // 我们可以把物品依次排列，整个问题就分解为了 n 个阶段，每个阶段对应一个物品怎么选择。先对第一个物品进行处理，选择装进去或者不装进去，然后再递归地处理剩下的物品。
+
+    public int maxW = Integer.MIN_VALUE; // 存储背包中物品总重量的最大值
+
+    // cw 表示当前已经装进去的物品的重量和；i 表示考察到哪个物品了；
+    // w 背包重量；items 表示每个物品的重量；n 表示物品个数
+    // 假设背包可承受重量 100，物品个数 10，物品重量存储在数组 a 中，那可以这样调用函数：
+    // f(0, 0, a, 10, 100)
+    public void f(int i, int cw, int[] items, int n, int w) {
+        if (cw == w || i == n) { // cw==w 表示装满了 ;i==n 表示已经考察完所有的物品
+            if (cw > maxW) maxW = cw;
+            return;
+        }
+        f(i + 1, cw, items, n, w); //当前物品不装进背包
+        if (cw + items[i] <= w) {// 已经超过可以背包承受的重量的时候，就不要再装了
+            f(i + 1, cw + items[i], items, n, w); //当前物品装进背包
+        }
+    }
+}
