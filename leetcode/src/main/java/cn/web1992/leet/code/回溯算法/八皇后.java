27991be7632369430f7b6827b5119dd08f7b2419
web1992 <554933654@qq.com>
2022-05-24 21:15:07 +0800

    add [八皇后.java]

---
+++ leetcode/src/main/java/cn/web1992/leet/code/回溯算法/八皇后.java
@@ -0,0 +1,63 @@
+package cn.web1992.leet.code.回溯算法;
+
+/**
+ * @author web1992
+ * @date 2022/4/30  22:27
+ * @link {https://time.geekbang.org/column/article/74287}
+ *
+ * <pre>
+ *     我们有一个 8x8 的棋盘，希望往里放 8 个棋子（皇后），每个棋子所在的行、列、对角线都不能有另一个棋子。
+ *     你可以看我画的图，第一幅图是满足条件的一种方法，第二幅图是不满足条件的。八皇后问题就是期望找到所有满足这种要求的放棋子方式。
+ * </pre>
+ */
+public class 八皇后 {
+
+    int[] result = new int[8];//全局或成员变量,下标表示行,值表示queen存储在哪一列
+
+    public static void main(String[] args) {
+
+        new 八皇后().cal8queens(0);
+    }
+
+    public void cal8queens(int row) { // 调用方式：cal8queens(0);
+        if (row == 8) { // 8个棋子都放置好了，打印结果
+            printQueens(result);
+            return; // 8行棋子都放好了，已经没法再往下递归了，所以就return
+        }
+        for (int column = 0; column < 8; ++column) { // 每一行都有8中放法
+            if (isOk(row, column)) { // 有些放法不满足要求
+                result[row] = column; // 第row行的棋子放到了column列
+                cal8queens(row + 1); // 考察下一行
+            }
+        }
+    }
+
+    private boolean isOk(int row, int column) {//判断row行column列放置是否合适
+        int leftup = column - 1, rightup = column + 1;
+        for (int i = row - 1; i >= 0; --i) { // 逐行往上考察每一行
+            if (result[i] == column) return false; // 第i行的column列有棋子吗？
+            if (leftup >= 0) { // 考察左上对角线：第i行leftup列有棋子吗？
+                if (result[i] == leftup) return false;
+            }
+            if (rightup < 8) { // 考察右上对角线：第i行rightup列有棋子吗？
+                if (result[i] == rightup) return false;
+            }
+            --leftup;
+            ++rightup;
+        }
+        return true;
+    }
+
+    private void printQueens(int[] result) { // 打印出一个二维矩阵
+        for (int row = 0; row < 8; ++row) {
+            for (int column = 0; column < 8; ++column) {
+                if (result[row] == column) System.out.print("Q ");
+                else System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+}
---
+++ leetcode/src/main/java/cn/web1992/leet/code/链表中环的入口节点.java
@@ -0,0 +1,13 @@
+package cn.web1992.leet.code;
+
+/**
+ * @author web1992
+ * @date 2022/4/16  22:46
+ * @link {https://www.bilibili.com/video/BV1R7411G78m}
+ */
+public class 链表中环的入口节点 {
+    
+    public static void main(String[] args) {
+
+    }
+}
